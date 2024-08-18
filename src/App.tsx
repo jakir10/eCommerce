3ef82db2d77b3725e36cf7036eee3fbb67d0448e
jakir10 <jakir.cse.bubt@gmail.com>
2024-08-18 17:22:34 +0600

    ecommerce design project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Category from './components/Category'
+import FeatureSectionFruits from './components/FeatureSectionFruits'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+      <main>
+        <Navbar/>
+        <Hero/>
+        <Category/>
+        <FeatureSectionFruits/>
+      </main>
+  )
+}
+
+export default App
