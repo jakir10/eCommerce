a9d2237d3990a079c750fc793331c882d6245319
jakir10 <jakir.cse.bubt@gmail.com>
2024-08-25 18:46:32 +0600

    ecommerce design almost done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import Banner from './components/Banner'
+import BlogSection from './components/BlogSection'
 import Category from './components/Category'
+import FeatureSectionBreakfast from './components/FeatureSectionBreakfast'
 import FeatureSectionFruits from './components/FeatureSectionFruits'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import NewsLetter from './components/NewsLetter'
 
 function App() {
 
@@ -11,6 +15,10 @@ function App() {
         <Hero/>
         <Category/>
         <FeatureSectionFruits/>
+        <FeatureSectionBreakfast/>
+        <Banner/>
+        <BlogSection/>
+        <NewsLetter/>
       </main>
   )
 }
